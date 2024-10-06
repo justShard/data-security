e8dece7bf405d47f89408c9b12cb9b44f5df26b9
Vitalii Mahdych <93186804+justShard@users.noreply.github.com>
2024-10-06 23:01:14 +0300

    Delete LR3.js

--- LR3.js
+++
@@ -1,9 +0,0 @@
-xor = (a,b)=>
-    {
-        var res = '';
-        for(var i=0; i<a.length; i++)
-            res += (parseInt(a[i], 16) ^ parseInt(b[i], 16)).toString(16)
-        return res;
-    }
-    
-    console.log(xor(xor(readline(),readline()),readline()).replace(/../g, x=>String.fromCharCode(parseInt(x,16))))
